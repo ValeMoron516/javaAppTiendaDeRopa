c73d6d995c5bcf9be67b014a03113bd66964c4fa
Valentín Morón <valemoron516@gmail.com>
2025-10-01 19:33:25 -0300

    primer commit relacionado al @proyectoTiendaDeRopa para el backend app con Java

---
+++ src/main/java/com/Tienda_de_Ropa/JavaAppTiendaRopa/JavaAppTiendaRopaApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_de_Ropa.JavaAppTiendaRopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppTiendaRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppTiendaRopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_de_Ropa/JavaAppTiendaRopa/JavaAppTiendaRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_de_Ropa.JavaAppTiendaRopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppTiendaRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
